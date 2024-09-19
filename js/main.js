1d4e69a2c3d63b6b2ae3f6dd97020decca5ad05c
kimyd123 <alwlsr112@naver.com>
2024-09-19 18:51:15 +0900

    포트폴리오 행성추가

--- js/main.js
+++ js/main.js
@@ -1,46 +1,41 @@
+// work 스크롤시 위치 오파시티 반응하는 박스
 
+      const io = new IntersectionObserver(entries => {
+          entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+              entry.target.classList.add('animated');
+              console.log('ddasdasdad');
+            }else{
+              entry.target.classList.remove('animated');
+              console.log('ddasdasdad');
+            }	
+          })
+        },{ threshold:[0, 0.6]});
 
-// 워크 부분 
+        const work_content_fig = document.querySelectorAll('.work_content_fig');
+        work_content_fig.forEach((el) => {
+          io.observe(el);
+          });
 
-
-const io = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.intersectionRatio > 0) {
-        entry.target.classList.add('animated');
-        console.log('ddasdasdad');
-      }else{
-        entry.target.classList.remove('animated');
-        console.log('ddasdasdad');
-      }	
-    })
-  },{ threshold:[0, 0.6]});
-
-  const work_content_fig = document.querySelectorAll('.work_content_fig');
-  work_content_fig.forEach((el) => {
-    io.observe(el);
-    });
-
-
+  // 스킬박스 요소 화면에 보일때 좌에서 우로
     
-
-    const io_skillbox = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (entry.intersectionRatio > 0) {
-            entry.target.classList.add('animated_skill_box');
-            console.log('등장');
-          }else{
-            entry.target.classList.remove('animated_skill_box');
-            console.log('퇴장');
-          }	
-        })
-      },{ threshold:[0, 0.6]});
+      const io_skillbox = new IntersectionObserver(entries => {
+          entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+              entry.target.classList.add('animated_skill_box');
+              console.log('등장');
+            }else{
+              entry.target.classList.remove('animated_skill_box');
+              console.log('퇴장');
+            }	
+          })
+        },{ threshold:[0, 0.8]});
 
       const about_skills_box = document.querySelector('.about_skills_box');
       io_skillbox.observe(about_skills_box);
 
 
-      //내비게이션바
-
+      //내비게이션바 휠 아리로할때 위로 숨겨짐
       const nav = document.querySelector('#header');
       const header_gnb = document.querySelector('.header_gnb');
       
@@ -52,3 +47,6 @@ const io = new IntersectionObserver(entries => {
         }
         console.log(e.deltaY);
       });
+
+
+
