a1a103332abbe25b8dcd896d508f2e830118c56e
kimyd123 <alwlsr112@naver.com>
2024-09-20 13:00:47 +0900

    스킬섹션 가로스크롤구현

--- js/main.js
+++ js/main.js
@@ -31,8 +31,8 @@
           })
         },{ threshold:[0, 0.8]});
 
-      const about_skills_box = document.querySelector('.about_skills_box');
-      io_skillbox.observe(about_skills_box);
+      // const about_skills_box = document.querySelector('.about_skills_box');
+      // io_skillbox.observe(about_skills_box);
 
 
       //내비게이션바 휠 아리로할때 위로 숨겨짐
@@ -50,47 +50,96 @@
 
 
 
+
+
+
 //트리거
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '#visual',  // 스크롤 트리거 요소
+    start: "top 10%",    // 시작 시점
+    end: "bottom top",   // 종료 시점
+    scrub: 2,            // 스크롤에 맞춰 부드럽게 진행
+    markers: false       // 디버깅 마커 표시 여부
+  }
+});
+
+tl.to(".gsap_item1", {
+  y: 500,  // y축으로 500px 이동
+});
+
+// 별도로 무한 반복 애니메이션 정의 (scrollTrigger와 무관)
+gsap.to(".gsap_item1", {
+  scale: 1.03,               // y축으로 10px 이동
+  duration: 2,         // 2초 동안 애니메이션
+  repeat: -1,          // 무한 반복
+  yoyo: true           // 처음 상태로 되돌아가는 애니메이션
+});
+
+
+const t2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '#about',  // 스크롤 트리거 요소
+    start: "top 40%",    // 시작 시점
+    end: "bottom top",   // 종료 시점
+    scrub: 2,            // 스크롤에 맞춰 부드럽게 진행
+    markers: false       // 디버깅 마커 표시 여부
+  }
+});
+
+gsap.to(".gsap_item2", {
+  rotate : 360,               // y축으로 10px 이동
+  duration: 500,         // 2초 동안 애니메이션
+  repeat: -1,          // 무한 반복
+  yoyo: true           // 처음 상태로 되돌아가는 애니메이션
+});
+
+t2.to(".gsap_item2", {
+  // x: '100%',
+  scale : 2,
+  opacity : 0,
+  scrub: 4,  
+});
+
+
+//가로 스크롤
+//matchMedia 반응형
+
 gsap.registerPlugin(ScrollTrigger);
-const gsap_item = document.querySelector('.gsap_item');
-const visual = document.querySelector('#visual');
 
-const gsap_item2 = document.querySelector('.gsap_item2');
-const gsap_item3 = document.querySelector('.gsap_item3');
-const work = document.querySelector('#work');
+const li = document.querySelectorAll('.about_skills li');
 
-gsap.to(gsap_item,{ 
-  x : -1000, 
-  y : 1000,
-  scale: 0.5,
-  scrollTrigger: {
-  trigger: gsap_item, // 요소가 뷰포트에 드러나는 순간부터 애니메이션이 작동
-  start: "top 10%", // 애니메이션 시작시점
-  end:"bottom top", // 애니메이션 종료시점
-  scrub: 2,
-  // markers: true 
-}}
-)
+ScrollTrigger.matchMedia({
+  '(min-width : 500px)' : function(){
+    // 트리거 작성
+    let list = gsap.utils.toArray(li);
 
-gsap.to(gsap_item2,{ 
-  y : 1000, 
-  scrollTrigger: {
-  trigger: work, // 요소가 뷰포트에 드러나는 순간부터 애니메이션이 작동
-  start: "top 50%", // 애니메이션 시작시점
-  end:"bottom top", // 애니메이션 종료시점
-  scrub: 1,
-  // markers: true 
-}}
-)
+    gsap.to(list, {
+      xPercent: -100 * (list.length - 1),  // 가로 스크롤 공식
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '.about_skills',
+        pin: true,            // 스크롤 중 요소 고정
+        pinSpacing: true,
+        scrub: 1,             // 스크롤에 따라 애니메이션 진행
+        start: 'center center',     // 시작 지점 설정 (뷰포트 상단)
+        end: '300%',        // 종료 시점 설정
+        markers: true         // 디버그용 마커
+      }
+    });
+  }
+});
 
-gsap.to(gsap_item3,{ 
-  y : 1000, 
-  scrollTrigger: {
-  trigger: work, // 요소가 뷰포트에 드러나는 순간부터 애니메이션이 작동
-  start: "10% 50%", // 애니메이션 시작시점
-  end:"bottom top", // 애니메이션 종료시점
-  scrub: 4,
-  markers: true 
-}}
-)
+
+
+
+
+
+
+
+
+
+
+
+
 
